4a88531f46425fe32e90e5ecf891f54feef005ff
Cristiano Sellitto <99354780+CristianoSellitto@users.noreply.github.com>
2023-12-13 13:26:16 -0500

    Update code.py
    Add wifi and JSON request

--- code.py
+++ code.py
@@ -1,6 +1,11 @@
 import time
+import ssl
 import alarm
 import board
+import socketpool
+import wifi
+import adafruit_requests
+import json
 from adafruit_magtag.magtag import MagTag
 
 # Add text display
@@ -16,8 +21,30 @@ magtag.add_text(
 magtag.peripherals.neopixel_disable = False
 magtag.peripherals.speaker_disable = True
 
+# Network SSH
+network_info = {
+    'ssid' : 'Room212',
+    'password' : 'temp',
+}
+
+# Connect to Wifi
+print("Attempting to connect to " + network_info["ssid"] + "...")
+wifi.radio.connect(network_info["ssid"], network_info["password"])
+print("Successfly connected to " + network_info["ssid"] + ".")
+print("IP address: ", wifi.radio.ipv4_address)
+
+pool = socketpool.SocketPool(wifi.radio)
+requests = adafruit_requests.Session(pool, ssl.create_default_context())
+
+MOUNTAIN_URL = "https://mtnpowder.com/feed?resortId=4"
+mountain_info = requests.get(MOUNTAIN_URL)
+
+current_temp = mountain_info.json()["CurrentConditions"]["Base"]["TemperatureC"]
+current_skies = mountain_info.json()["CurrentConditions"]["Base"]["Skies"]
+current_date = mountain_info.json()["Forecast"]["OneDay"]["date"]
+
 # Add text on screen
-magtag.set_text("\ntext")
+magtag.set_text("\nWeather: " + current_temp + "ºC")
 magtag.peripherals.neopixels.brightness = 0.1
 magtag.peripherals.neopixels.fill((0, 255, 0))
 
@@ -27,10 +54,10 @@ while True:
 
     if button_one == True:
         magtag.set_text("\nSleep")
-        # Sleep until the alarm goes off (if button D11 is pressed or after 5 seconds)
+        # Sleep until the alarm goes off (if button D11 is pressed or after 300 seconds (5 minutes))
         magtag.peripherals.deinit()
         pin_alarm = alarm.pin.PinAlarm(pin=board.D11, value=False, pull=True)
-        time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 5)
+        time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 300)
         alarm.exit_and_deep_sleep_until_alarms(pin_alarm, time_alarm)
     elif button_two == True:
         magtag.set_text("\nTest2\ntest2?")
