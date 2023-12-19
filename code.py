3db99301ef2f29d513ca3081a645650734b203f1
Cristiano Sellitto <99354780+CristianoSellitto@users.noreply.github.com>
2023-12-19 13:26:57 -0500

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,6 @@
 - Created December 2023
 
 This program write the current weather in Mont Tremblant to a Adafruit MagTag
-
 '''
 
 import time
@@ -18,6 +17,15 @@ import microcontroller
 import adafruit_requests
 from adafruit_magtag.magtag import MagTag
 
+# Writes weather data to the screen
+def set_weather_data(place_name, date, temperature, wind_chill, skies):
+    magtag.set_text(
+        "\n" + str(place_name) + " (" + str(date) +
+        ")\nWeather: " + str(temperature) +
+        " C\nWind Chill: " + str(wind_chill) +
+        " C\nSkies: " + str(skies)
+    )
+
 # Puts the device to a deep sleep
 def sleep_device():
     # Sleep until the alarm goes off (if button D11 is pressed or after 3600 seconds (1 hour))
@@ -29,6 +37,9 @@ def sleep_device():
 # Add text display
 magtag = MagTag()
 
+# Disabling unused features for battery saving
+magtag.peripherals.speaker_disable = True
+
 # Import Network SSH
 from secrets import secrets
 
@@ -38,9 +49,9 @@ try:
     magtag.add_text(
         text_position=(
                 1,
-                (magtag.graphics.display.height // 2) - 20,
+                (magtag.graphics.display.height // 2) - 25,
             ),
-            text_scale=1,
+            text_scale=1.5,
     )
     # Turn LEDS on and set the colour to blue
     magtag.peripherals.neopixels.brightness = 0.1
@@ -50,8 +61,9 @@ try:
     # Connect to Wi-Fi
     wifi.radio.connect(secrets["ssid"], secrets["password"])
 except:
+    # Set the colour of the LEDs to red
     magtag.peripherals.neopixels.fill((255, 0, 0))
-    magtag.set_text("\nError connecting to " + secrets["ssid"] + ".\nRestarting in 10 seconds.")
+    magtag.set_text("\nError connecting to\n" + secrets["ssid"] + ".\nRestarting in 10 seconds.")
     # Wait 10 seconds
     time.sleep(10)
     # Reset program
@@ -60,61 +72,63 @@ else:
     print("Successfly connected to " + secrets["ssid"] + ".")
     print("IP address: ", wifi.radio.ipv4_address)
 
-# Set text formatting
-magtag.add_text(
-    text_position=(
-        1,
-        (magtag.graphics.display.height // 2) - 25,
-    ),
-    text_scale=1.5,
-)
-
-# Disabling unused features for battery saving
-magtag.peripherals.speaker_disable = True
-
-# Begin HTTP request
-pool = socketpool.SocketPool(wifi.radio)
-requests = adafruit_requests.Session(pool, ssl.create_default_context())
-
-# Get Mount Tremblant Weather Information
+# Get Mount Tremblant Weather JSON file
 try:
+    # Set the colour of the LEDs to purple
+    magtag.peripherals.neopixels.fill((255, 0, 255))
+    magtag.set_text("\nObtaining information...")
+    # Begin request
+    pool = socketpool.SocketPool(wifi.radio)
+    requests = adafruit_requests.Session(pool, ssl.create_default_context())
+    # Request file
     MOUNTAIN_URL = "https://mtnpowder.com/feed?resortId=4"
     mountain_info = requests.get(MOUNTAIN_URL)
+    # Get Current Weather information
+    current_temp = math.ceil(float(mountain_info.json()["CurrentConditions"]["Base"]["TemperatureC"]))
+    current_chill = math.ceil(float(mountain_info.json()["CurrentConditions"]["Base"]["WindChillC"]))
+    current_skies = mountain_info.json()["CurrentConditions"]["Base"]["Skies"]
+    current_date = mountain_info.json()["Forecast"]["OneDay"]["date"]
 except:
-    magtag.set_text("\nError getting weather information.\nRestarting in 10 seconds.")
+    magtag.peripherals.neopixels.fill((255, 0, 0))
+    magtag.set_text("\nError obtaining the weather.\nRestarting in 10 seconds.")
     time.sleep(10)
     microcontroller.reset()
-current_temp = math.ceil(float(mountain_info.json()["CurrentConditions"]["Base"]["TemperatureC"]))
-current_chill = math.ceil(float(mountain_info.json()["CurrentConditions"]["Base"]["WindChillC"]))
-current_skies = mountain_info.json()["CurrentConditions"]["Base"]["Skies"]
-current_date = mountain_info.json()["Forecast"]["OneDay"]["date"]
+
+# Set the colour of the LEDs to green
+magtag.peripherals.neopixels.fill((0, 255, 0))
+
+'''
+# Prompt to select a day to request
+timer = 5
+day_requesting = 0
+while timer != 0:
+    # Set the colour of the LEDs to yellow
+    magtag.peripherals.neopixels.fill((255, 255, 0))
+    button_D14_pressed = magtag.peripherals.button_b_pressed
+    button_D12_pressed = magtag.peripherals.button_c_pressed
+
+    # Change the day being requested if a button is pressed
+    if button_D14_pressed == True:
+        day_requesting = day_requesting + 1
+    elif button_D12_pressed == True:
+        day_requesting = day_requesting - 1
+    magtag.set_text(
+        "\nDay Requesting: " + str(day_requesting) + "\n" +
+        "Time until request: " + str(timer)
+    )
+    time.sleep(1)
+    timer = timer - 1
+'''
 
 # Add text on screen
-magtag.refresh()
-magtag.set_text(
-    "\nVillage Info\nWeather: " + str(current_temp) +
-    " C\nWind Chill: " + str(current_chill) +
-    " C\nLast Update: " + str("date")
+set_weather_data(
+    "Village",
+    current_date,
+    current_temp,
+    current_chill,
+    current_skies
 )
 
-# Turn LEDS to green and sleep after 15 seconds
-magtag.peripherals.neopixels.fill((0, 255, 0))
+# Sleep after 15 seconds
 time.sleep(15)
 sleep_device()
-
-while True:
-    button_one_pressed = magtag.peripherals.button_a_pressed
-
-    if button_one_pressed == True:
-        magtag.set_text("\nTest2\ntest2?")
-    
-    '''
-    magtag.set_text("\ntest")
-    time.sleep(1)
-    magtag.set_text("\nTest2\ntest2?")
-    time.sleep(1)
-    magtag.set_text("\nTest3\ntest3\ntest3?")
-    time.sleep(1)
-    magtag.set_text("\nTest4\ntest4\ntest4\ntest4?")
-    time.sleep(1)
-    '''
